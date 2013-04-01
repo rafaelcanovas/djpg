a6cd85b13ca7948449b5714289bbe0992133e174
Rafael Canovas <rafaelcanovas@me.com>
2013-04-01 15:25:21 -0300

    Tweaked a little setup.py

--- setup.py
+++ setup.py
@@ -2,17 +2,18 @@
 from setuptools import setup
 
 setup(name='djpg',
-      version='0.1.1',
-      author='Rafael Canovas',
-      author_email='rafaelcanovas@me.com',
-      url='https://github.com/rafaelcanovas/djpg',
-      long_description=open('README.rst').read(),
-      packages=['djpg'],
-      package_data={'': ['UNLICENSE']},
-      install_requires=[
-            'requests==0.14.0',
-            'furl==0.3.2',
-            'xmltodict==0.2'
-      ],
-      license=open('UNLICENSE').read(),
+	version='0.1.1',
+	description='djpg is a Django module that integrates with the online payment service PagSeguro.',
+	long_description=open('README.rst').read(),
+	author='Rafael Canovas',
+	author_email='rafaelcanovas@me.com',
+	url='https://github.com/rafaelcanovas/djpg',
+	packages=['djpg'],
+	package_data={'': ['UNLICENSE']},
+	install_requires=[
+		'requests==0.14.0',
+		'furl==0.3.2',
+		'xmltodict==0.2'
+	],
+	license=open('UNLICENSE').read(),
 )
