4b87990b863f7829c5d8bbb7a5c3f427bc2bfcec
Rafael Canovas <rafaelcanovas@me.com>
2013-10-16 21:58:24 -0300

    Updated setup.py package_data

--- setup.py
+++ setup.py
@@ -12,7 +12,8 @@ except (ImportError, OSError, IOError):
 	long_description = ''
 
 
-setup(name='djpg',
+setup(
+	name='djpg',
 	version='0.1.1',
 	description='djpg is a Django module that integrates with the online payment service PagSeguro.',
 	long_description=long_description,
@@ -20,7 +21,7 @@ setup(name='djpg',
 	author_email='rafaelcanovas@me.com',
 	url='https://github.com/mstrcnvs/djpg',
 	packages=['djpg'],
-	package_data={'': ['UNLICENSE']},
+	package_data={'': ['LICENSE']},
 	install_requires=[
 		'requests',
 		'xmltodict==0.5.1'
